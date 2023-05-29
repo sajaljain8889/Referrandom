717fc22b1864fd685ab11a70c573b4de054b05fa
Sajal Jain <sajaljain7415@gmail.com>
2023-05-29 15:01:40 +0530

    First comit

---
+++ src/main/java/com/example/referandom/ReferandomApplication.java
@@ -0,0 +1,13 @@
+package com.example.referandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferandomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferandomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/referandom/ReferandomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.referandom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferandomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
